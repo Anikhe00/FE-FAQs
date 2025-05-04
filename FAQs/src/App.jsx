24fd4c3c9921ee1e03994d3015d5632ad555a58e
Shakirat Akanji <akanjishakirato@gmail.com>
2025-05-04 22:48:43 +0100

    Responsiveness

--- FAQs/src/App.jsx
+++ FAQs/src/App.jsx
@@ -2,18 +2,21 @@ import FaqAccordion from "./FaqAccordion.jsx"
 import { useState } from "react"
 import faqData from "./data.js"
 import StarIcon from "./assets/ph_star-four-fill.svg"
+import VectorLine from "./assets/Vector.svg"
 
 function App() {
   const [activeId, setActiveId] = useState(1); 
 
 return (
-  <div className="bg-[#F8EEFF] w-full h-dvh flex flex-col items-center justify-center">
-    <div className="bg-[#301534] w-full h-[320px] absolute top-0 left-0"></div>
-    <div className="z-10 relative w-full max-w-[560px] bg-white shadow-[0px_32px_56px_rgba(80,0,118,0.10)] p-10 h-auto flex flex-col gap-8 rounded-2xl">
+  <div className="bg-[#F8EEFF] w-full h-dvh flex flex-col pr-6 pl-6 items-center justify-center">
+    <div className="bg-[#301534] flex justify-center items-center lg:h-[320px] md:h-[320px] h-[240px] absolute top-0 overflow-hidden left-0">
+      <img className="w-[131rem] h-[43.4rem] object-none" src={VectorLine} alt="Vector" />
+    </div>
+    <div className="z-10 relative w-full lg:max-w-[560px] md:max-w-[560px] bg-white shadow-[0px_32px_56px_rgba(80,0,118,0.10)  p-6 lg:p-10 h-auto flex flex-col lg:gap-8 md:gap-8 gap-6 lg:rounded-2xl md:rounded-2xl rounded-lg">
 
       <div className="flex flex-row items-center gap-4 h-auto">
-        <img src={StarIcon} size="40px" alt="Star" />
-        <h1 className="text-[56px] font-bold text-[#301534] leading-[64px]">FAQs</h1>
+        <img className="lg:size-[40px] md:size[40px] size-[32px]" src={StarIcon} alt="Star" />
+        <h1 className="lg:text-[56px] md:text-[56px] text-[32px] font-bold text-[#301534] lg:leading-[64px] leading-[40px]">FAQs</h1>
       </div>
 
     <div className="flex flex-col w-full h-auto">
@@ -26,9 +29,9 @@ return (
           />
           {index !== faqData.length - 1 && (
             <>
-              <div className="h-5" />
+              <div className="lg:h-5 md:h-5 h-4" />
               <div className="h-[1px] bg-[#DDB2FF] opacity-20" />
-              <div className="h-5" />
+              <div className="lg:h-5 md:h-5 h-4" />
             </>
           )}
         </div>
--- FAQs/src/FaqAccordion.jsx
+++ FAQs/src/FaqAccordion.jsx
@@ -9,14 +9,17 @@ export default function FaqAccordion({ question, answer, isExpanded, onToggle })
   return (
     <div className="flex w-full flex-col gap-3 ">
       <div onClick={handleToggle} className="flex flex-row justify-between items-center w-full cursor-pointer group">
-        <h2 className="text-[#301534] font-semibold text-lg transition-colors duration-200 group-hover:text-[#AD28EB]">{question}</h2>
+        <h2 className="text-[#301534] font-semibold lg:text-lg md:text-lg text-base transition-colors duration-200 group-hover:text-[#AD28EB]">{question}</h2>
         <img 
         src={isExpanded ? MinusIcon : PlusIcon}
         alt={isExpanded ? "Minus" : "Plus"}
         />
       </div>
 
-      {isExpanded && <p className="text-[#8B6990] text-base font-normal w-full">{answer}</p>}
+      <div className={`transition-all duration-300 overflow-hidden ${isExpanded ? "opacity-100 max-h-999px" : "opacity-0 max-h-0"}`}>
+      <p className="text-[#8B6990] lg:text-base md:text-base text-sm font-normal w-full">{answer}</p>
+      </div>
+
     </div>
   )
 }
